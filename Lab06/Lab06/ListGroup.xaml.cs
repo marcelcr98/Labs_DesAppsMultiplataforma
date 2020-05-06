76deb726d4c4fa126f31c5082316045bc6f39fef
marceloarmando <mcuellarr98@gmail.com>
2020-05-05 20:30:00 -0500

    commit 1

---
+++ Lab06/Lab06/ListGroup.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Lab06
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ListGroup : ContentPage
+    {
+        private List<UserList> _listOfUsers;
+        public List<UserList> ListOfUsers { get { return _listOfUsers; } set { _listOfUsers = value; base.OnPropertyChanged(); } }
+
+
+        public ListGroup()
+        {
+            InitializeComponent();
+            var dList = new UserList()
+            {
+                new User() { FirstName = "Marcelo", LastName = "Cuellar" },
+                new User() { FirstName = "Juan", LastName = "Saladino" },
+                new User() { FirstName = "Cloy", LastName = "Clotilde" }
+            };
+            dList.Heading = "D";
+
+            var bList = new UserList()
+            {
+                new User() { FirstName = "Diego", LastName = "Torres" }
+            };
+            bList.Heading = "B";
+
+            var lList = new UserList()
+            {
+                new User() { FirstName = "Bob", LastName = "Patiño" }
+            };
+            lList.Heading = "L";
+
+            var list = new List<UserList>()
+            {
+                dList,
+                bList,
+                lList
+            };
+            Console.WriteLine(list);
+            ListOfUsers = list;
+        }
+    }
+}
---
+++ Lab06/Lab06/Users.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab06
+{
+    public class User
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string DisplayName
+        {
+            get
+            {
+                return $"{LastName}, {FirstName}";
+            }
+        }
+    }
+
+    public class UserList : List<User>
+    {
+        public string Heading { get; set; }
+        public List<User> Persons => this;
+    }
+
+
+}
